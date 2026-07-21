78576ffc8d989a6c1dcda0b4e05276c2c57d2904
rekunii <rekunii@example.com>
2026-07-21 20:38:56 +0900

    暑スギ

--- ms_shellink.cr
+++ ms_shellink.cr
@@ -12,47 +12,80 @@ file = File.open("text.txttxttxt")
 # printf("%x", link_flags)
 link_flags : UInt32
 
-# # -------- LinkFlags (ビット位置は合ってるか不明…)
-# has_link_target_idList =
-# has_link_Info =
-# has_name =
-# has_relative_path =
-has_workingDir    = true  # 0x10  <-これらはリトル
-has_arguments     = false # 0x20    エンディアンとして
-has_iconLocation  = false # 0x40    の表記
-is_unicode        = true  # 0x80
-# force_no_linkInfo = true  # 0x01
-# has_expString     = true  # 0x02
-# run_in_separateProcess = false #0x04
-# # undefined1 0x08
+# -------- LinkFlags
+has_link_target_idList = true
+has_link_Info = true
+has_name = false
+has_relative_path = true
+has_workingDir    = true
+has_arguments     = false
+has_iconLocation  = false
+is_unicode        = true
 
-# has_darwin_id = false          # 0x00_10
-# run_as_user   = false          # 0x00_20
-# has_expIcon   = false          # 0x00_40
-# no_pid_alias  = false          # 0x00_80
-# # unused2                      # 0x00_01
-# run_with_ShimLayer     = false # 0x00_02
-# force_no_linkTrack     = false # 0x00_04
-# enable_target_metadata = false # 0x00_08
+force_no_linkInfo = false # I
+has_expString     = false
+run_in_separateProcess = false
+# Unused1
+has_darwin_id = false # M
+run_as_user   = false
+has_expIcon   = false
+no_pid_alias  = false
 
-# disable_link_pathTracking = false       # 0x00_00_10
-# disable_known_folder_tracking = false   # 0x00_00_20
-# disable_known_folder_allias = false   # 0x00_00_40
-# allow_link_to_link = false            # 0x00_00_80
-# unalias_os_save = false               # 0x00_00_01
-# prefer_environment_path = false       # 0x00_00_02
-# keep_localIdListForUNCTarget = false  # 0x00_00_04
-# # --------
+# Unused2 # Q
+run_with_ShimLayer     = false # R
+force_no_linkTrack     = false
+enable_target_metadata = true
+
+disable_link_pathTracking = false     # U
+disable_known_folder_tracking = false # V
+disable_known_folder_allias = false   # W
+allow_link_to_link = false            # X
+unalias_on_save = false               # Y
+prefer_environment_path = false       # Z
+keep_localIdListForUNCTarget = false  # AA
+# --------
 
 link_flags = (
-  (if has_workingDir;0x10 else 0 end) |
-  (if has_arguments;0x20 else 0 end) |
-  (if has_iconLocation; 0x40 else 0 end) |
-  (if is_unicode;0x80 else 0 end) |
+  # (if has_workingDir;0x10 else 0 end) |
+  # (if has_arguments;0x20 else 0 end) |
+  # (if has_iconLocation; 0x40 else 0 end) |
+  # (if is_unicode;0x80 else 0 end) |
   # (if ;0x040000 else 0 end) | ファイル追跡無効
-  0
+  (has_link_target_idList.to_unsafe <<  0) |
+  (has_link_Info.to_unsafe          <<  1) |
+  (has_name.to_unsafe               <<  2) |
+  (has_relative_path.to_unsafe      <<  3) |
+  (has_workingDir.to_unsafe         <<  4) |
+  (has_arguments.to_unsafe          <<  5) |
+  (has_iconLocation.to_unsafe       <<  6) |
+  (is_unicode.to_unsafe             <<  7) | # 0x000000_80
+  (force_no_linkInfo.to_unsafe      <<  8) |
+  (has_expString.to_unsafe          <<  9) |
+  (run_in_separateProcess.to_unsafe << 10) |
+  #Unused1
+  (has_darwin_id.to_unsafe << 12) |
+  (run_as_user.to_unsafe << 13) |
+  (has_expIcon .to_unsafe << 14) |
+  (no_pid_alias .to_unsafe << 15) |
+  # Unused2
+  (run_with_ShimLayer .to_unsafe << 17) |
+  (force_no_linkTrack .to_unsafe << 18) |
+  (enable_target_metadata.to_unsafe << 19) |
+  (disable_link_pathTracking.to_unsafe << 20) |
+  (disable_known_folder_tracking.to_unsafe << 21) |
+  (disable_known_folder_allias.to_unsafe << 22) |
+  (allow_link_to_link.to_unsafe << 23) |
+  (unalias_on_save.to_unsafe << 24) |
+  (prefer_environment_path.to_unsafe << 25) |
+  (keep_localIdListForUNCTarget.to_unsafe << 26)
 ).to_u32
-# printf("0x%04x", link_flags)
+printf("0x%08X\n", link_flags)
+
+# puts (true.to_unsafe  << 4)
+
+exit
+
+link_flags = 0x00_08_00_9B_u32 # Windows 11 日本語での初期リンクフラグ
 
 File.open("text_lnk", "wb") do |lnk|
   # 00-03 (4 bytes) HeaderSize
@@ -65,7 +98,7 @@ File.open("text_lnk", "wb") do |lnk|
     0x00, 0x00, 0x00, 0x00, 0x00, 0x46
   ])
   # 14-17 (4 bytes) LinkFlags
-  lnk.write_bytes(link_flags)
+  lnk.write_bytes(link_flags, IO::ByteFormat::LittleEndian)
   # 18-1b (4 bytes) FileAttributes
   lnk.write(Bytes[0x20, 0x00, 0x00, 0x00]) # FILE_ATTRIBUTE_NORMAL
   
@@ -86,5 +119,7 @@ File.open("text_lnk", "wb") do |lnk|
   # 40-41 (2 bytes) HotKey
   lnk.write(ZEROS_2)  
   printf("%#x\n", lnk.pos)
-  #  
+  # 42-4B (2-4-4 bytes)  ReServed 1-2-3
+  lnk.write(Bytes.new(10, 0))
+  # 4C-4D (2 bytes) IDListSize
 end
